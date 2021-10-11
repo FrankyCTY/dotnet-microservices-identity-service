2422078893f7d9e3b9b14bb4c4169c7bbfb31bf8
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-10-12 00:25:46 +1300

    init

---
+++ src/Play.Identity.Service/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace Play.Identity.Service.Entities
+{
+    [CollectionName("Users")]
+    public class ApplicationUser : MongoIdentityUser<Guid>
+    {
+        // money
+        public decimal Gil { get; set; }
+        
+    }
+}
